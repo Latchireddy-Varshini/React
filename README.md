89d5144e35ed71be0d98a54ab2ddef70c676dfaf
LATCHIREDDY SAI VARSHINI <saivarshini2102@gmail.com>
2024-08-21 21:37:47 +0530

    parcel and babel intro

---
+++ README.md
@@ -0,0 +1,20 @@
+# Namaste React  
+
+# Parcel
+- Dev Build
+- Local Server
+- File Watching Algorithm-written in c++
+- HMR(Hot Module Replacement)
+- Caching-Faster Builds
+- Image Optimisation
+-  Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling-Support old browsers
+- Diagnostic
+- Error Handling
+- HTTP's
+- Tree Shaking-Removes unused code
+- Different dev and Prod bundles
--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-const heading=React.createElement("h1",{id:"head"},"Welcome to the React");
-    const root=ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+import React  from "react";
+import ReactDOM from "react-dom/client";
+
+ //react element
+// const jsxheading=<h1 className="heading">Welcome to React</h1>
+    
+
+//react component
+// const HeadingComponent=()=>{
+//    return <h1>Functional Component</h1>
+// };
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<HeadingComponent/>);
+
+
+//Component Composition
+const Title=()=>(
+    <h1>Namaste React using Jsx</h1>
+);
+const HeadingComponent=()=>(
+    <div id="container">
+    <Title/>
+    <h1>Functional Component</h1>
+    </div>
+);
+ const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);
